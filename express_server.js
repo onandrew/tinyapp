123d060ce3c1912112cace638e4ca790c8c58b5b
Andrew On <a_on93@hotmail.com>
2023-09-30 12:06:44 -0400

    add get route

--- express_server.js
+++ express_server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs")
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -27,6 +28,9 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
