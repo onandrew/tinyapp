7a6ca83b758ce96f35cded866cde60716548c78e
Andrew On <a_on93@hotmail.com>
2023-10-08 13:15:46 -0400

    add feature where only users can edit or delete their own urls

--- express_server.js
+++ express_server.js
@@ -7,6 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(cookie());
 
+//generate randomString for unique userID and urls
 function generateRandomString() {
   let randomString = "";
   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -16,6 +17,7 @@ function generateRandomString() {
   return randomString;
 }
 
+//database of users
 const users = {
   userRandomID: {
     id: "userRandomID",
@@ -29,11 +31,13 @@ const users = {
   },
 };
 
+//database for urls
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userId: "userRandomID" },
+  "9sm5xK": { longURL: "http://www.google.com", userId: "user2RandomID" }
 };
 
+//function to add a new user after registration
 const addNewUser = (email, password, database) => {
   const id = generateRandomString();
   database[id] = {
@@ -44,6 +48,7 @@ const addNewUser = (email, password, database) => {
   return id;
 };
 
+//checking if a user is in our database
 const checkIfUserExists = (email) => {
   for (let x in users) {
     if (users[x][email] === email) {
@@ -53,6 +58,7 @@ const checkIfUserExists = (email) => {
   return true;
 };
 
+//retrieve userID through user email
 const getUserByEmail = (email, database) => {
   for (let userId in database) {
     if (email === database[userId].email) {
@@ -62,7 +68,17 @@ const getUserByEmail = (email, database) => {
   return undefined;
 };
 
+const urlsForUser(id) => {
+  const usersUrls = {};
+  for (let x in users) {
+    if (id === users[x].userId) {
+      usersUrls[x]= user[x];
+    }
+  }
+  return usersUrls;
+}
 
+//default page
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -73,14 +89,29 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase, user: users[req.cookies["user_id"]]};
+  let user = users[req.cookies["userId"]];
+  if (!user) {
+    res.send("Please sign in or register");
+    res.redirect("urls_login");
+  }
+  else {
+  const templateVars = { 
+    urls: urlsForUser(req.cookies.userId),
+    user: users[req.cookies["userId"]]
+  };
   res.render("urls_index", templateVars);
+}
 });
 
 app.post('/urls', (req, res) => {
+  if (!req.cookies['user_id']) {
+    res.statusCode(403).send('You must be logged in to do that');
+  }
+  else {
   const shortURL = generateRandomString();
-  urlDatabase[shortURL] = req.body.longURL;
+  urlDatabase[req.params.id].longURL = req.body.longURL;
   res.redirect(`/urls/${shortURL}`);
+  }
 })
 
 app.get("/urls/new", (req, res) => {
@@ -93,9 +124,9 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get('/u/:shortURL', (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL].longURL;
   if (longURL) {
-    res.redirect(urlDatabase[req.params.shortURL]);
+    res.redirect(urlDatabase[req.params.shortURL].longURL);
   } else {
     res.statusCode(404).send('This URL does not exist');
   }
@@ -104,37 +135,57 @@ app.get('/u/:shortURL', (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL],
+    longURL: urlDatabase[req.params.shortURL].longURL,
     user: req.cookies["user_id"]
   };
   res.render("urls_show", templateVars);
 });
 
 app.post('/urls/:id', (req, res) => {
+  if (urlDatabase[req.params.id].userID === req.cookies["userID"]) {
   let longURL = req.body.longURL
   urlDatabase[req.params.id] = longURL;
   res.redirect('/urls');
+  }
+  else {
+    res.status(403).send("You are not authorized to edit this URL. Please login as the appropriate user");
+    res.redirect('/urls/login');
+  }
 })
 
 app.post("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.id;
-  const longURL = urlDatabase[shortURL];
+  const longURL = urlDatabase[shortURL].longURL;
   res.redirect(longURL);
 });
 
 app.post("/urls/:shortURL/delete", (req,res) => {
   const shortURL = req.params.shortURL;
-  delete urlDatabase[shortURL];
-  res.redirect("/urls");
+  if (req.cookies['user_id'] === urlDatabase[shortURL].userId) {
+    delete urlDatabase[shortURL];
+    res.redirect("/urls");
+  }
+  else {
+    res.status(403).send("You are not authorized to delete this URL. Please login as the appropriate user");
+  }
 });
 
 app.post("/urls/:shortURL/edit", (req, res) => {
   const key = req.params.shortURL;
+  if (req.cookies['user_id'] === urlDatabase[key].userId) {
   urlDatabase[key] = req.body.longURL;
   res.redirect('/urls')
+  } else {
+    res.status(403).send("You are not authorized to edit this URL. Please login as the appropriate user");
+  }
 });
 
 app.get("/login", (req, res) => {
+  if (req.cookies.userId) {
+    res.redirect('/urls');
+    return;
+  }
+
   let templateVars = {user: null};
   res.render("urls_login", templateVars);
 });
@@ -161,6 +212,10 @@ app.post("/logout", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
+  if (req.cookies.userId) {
+    res.redirect('/urls');
+    return;
+  }
   let templateVars = {user: users[req.cookies["user_id"]]};
   res.render("urls_registration", templateVars);
   res.redirect("/urls");
